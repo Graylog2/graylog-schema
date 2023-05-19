1e969f4a591b3d2e41a4ea8acabc25a835f3bd06
Michael Wenthold <michael.wenthold@graylog.com>
2023-05-19 12:00:47 -0400

    Update version to 3.3

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2015-2023 Graylog, Inc'
 # author = 'Nick Carstensen'
 
 # The full version, including alpha/beta/rc tags
-release = '3.2'
+release = '3.3'
 
 
 # -- General configuration ---------------------------------------------------
